a1244cb94b6dfe5f49983c000af657a039318a46
gids-dadi <gideons564@gmail.com>
2022-04-15 12:23:02 +0100

    Add desktop version of the about page

--- index.js
+++ index.js
@@ -1,80 +1,110 @@
+// const logoClass = document.querySelector('.logo-icon');
+
+// Mobile Menu
+const hamburger = document.querySelector('#menu-mobile');
+const menuLinks = document.querySelector('.nav-items');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  menuLinks.classList.toggle('is-active');
+});
+// logoClass.classList.toggle('closeLogo');
+
+const aboutTia = document.querySelector('.about-link');
+const programTia = document.querySelector('.program-link');
+const joinTia = document.querySelector('.join-link');
+const sponsorTia = document.querySelector('.sponsor-link');
+const newsTia = document.querySelector('.news-link');
+
+const close = () => {
+  hamburger.classList.toggle('is-active');
+  menuLinks.classList.toggle('is-active');
+};
+
+aboutTia.addEventListener('click', () => {
+  close();
+});
+
+programTia.addEventListener('click', () => {
+  close();
+});
+
+joinTia.addEventListener('click', () => {
+  close();
+});
+
+sponsorTia.addEventListener('click', () => {
+  close();
+});
+
+newsTia.addEventListener('click', () => {
+  close();
+});
+
 const speakers = [
-{
-  image: 'images/p1.jpg',
-  name: 'Yochai Benkler',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-},
+  {
+    image: 'images/p1.jpg',
+    name: 'Yochai Benkler',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
 
-{
-  image: 'images/p2.jpg',
-  name: 'SohYeong Noh',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-},
+  {
+    image: 'images/p2.jpg',
+    name: 'SohYeong Noh',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
 
-{
-  image: 'images/p3.jpg',
-  name: 'Lila Tratilkov',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-},
+  {
+    image: 'images/p3.jpg',
+    name: 'Lila Tratilkov',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
 
-{
-  image: 'images/p4.jpg',
-  name: 'Kilnam Chon',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-},
+  {
+    image: 'images/p4.jpg',
+    name: 'Kilnam Chon',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
 
-{
-  image: 'images/p5.jpg',
-  name: 'Julia Leda',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-},
+  {
+    image: 'images/p5.jpg',
+    name: 'Julia Leda',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
 
-{
-  image: 'images/p6.jpg',
-  name: 'Ryan Merkley',
-  position: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-  brief: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium.',
-}
-]
-
+  {
+    image: 'images/p6.jpg',
+    name: 'Ryan Merkley',
+    position: 'Lorem ipsum dolor sit amet consectetur, adipisicing.',
+    brief:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo praesentium quo laudantium Lorem ipsum dolor sit amet consectetur.',
+  },
+];
 
 for (let i = 0; i < speakers.length; i += 1) {
- document.querySelector('.speakers-list').innerHTML += `
-<div class='row m-2'>
+  document.querySelector('.speakers-list').innerHTML += `
 
-<div  class = 'col' > 
+<div class='row justify-content-md-center'>
+
+<div  class = 'col-md-4' > 
 <img src = ${speakers[i].image} />
-
 </div>
 
-  <div class="col">
+  <div class="col-md-4">
   <h2>${speakers[i].name}</h2>
     <p class='text-danger'>${speakers[i].position}</p>
     <p>${speakers[i].brief}</p>
 </div>
 
-    </div>`;
+</div>`;
 }
-
-/* <div class='></div>
-    <div class="work-">
-      <h2>${cardDetails[0].title}</h2>
-      <p>${cardDetails[0].cardText}</p>
-      <div class "tech-list ">
-               <ul>
-                    <li>${techList}</li>
-                </ul>
-               </div>
-              <button
-               class="hint-button works-button"
-               type="button"
-               data-index="${i}"
-            >
-               See Project
-             </button>
-                </div> */
